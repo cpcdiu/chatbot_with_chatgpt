cdea8df5e4293d4c2bbe0823ee20c14fd5cec916
CoderTahmid <tahmid.ibne@gmail.com>
2025-05-04 09:24:44 +0600

    project setup done with ReacJS. Also installed TailwindCSS and DaisyUI Component Library

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='text-6xl'>king</p>
+      <button className='btn btn-ghost'>okk</button>
+    </>
+  )
+}
+
+export default App
