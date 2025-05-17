ef27c6f519e25bd9d86f160e5a32d729bc8b3c70
CoderTahmid <tahmid.ibne@gmail.com>
2025-05-17 11:31:57 +0600

    A dynamic sedning button added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
+import { HiArrowCircleUp } from "react-icons/hi";
 
 const Home = () => {
     const [inputVal, setInputVal] = useState();
-    console.log(typeof inputVal);
-        
+
     return (
         <>
-            <input onChange={(e) => setInputVal(e.target.value)} value={inputVal} type="text" placeholder="What's on you mind? ☺️" className="input input-xl h-[90px] -translate-x-1/2 left-1/2 w-[60%] mx-auto rounded-3xl top-[400px] fixed" />
+            <div className='top-[400px] fixed flex items-center justify-center w-full'>
+                <div className='flex justify-center items-center lg:w-[80%] sm:w-[90%] mx-auto'>
+                    <input
+                        onChange={(e) => setInputVal(e.target.value)}
+                        type="text"
+                        placeholder="What's on you mind? ☺️"
+                        className="input input-xl h-[90px] rounded-3xl flex-1/2"
+                    />
+                    <HiArrowCircleUp className={`text-7xl ${inputVal ? "" : "hidden"} hover:cursor-pointer`}></HiArrowCircleUp>
+                </div>
+            </div>
         </>
     );
 };
